68ff841a3d2af27597f7a9f593ac0e7c489d183f
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-08-31 18:04:22 +0200

    Create rows method implemented

--- DynamicGridTutorial/MainWindow.xaml.cs
+++ DynamicGridTutorial/MainWindow.xaml.cs
@@ -20,13 +20,50 @@ namespace DynamicGridTutorial
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Grid DynamicGrid;
+
         public MainWindow()
         {
             InitializeComponent();
+            CreateGrid();
         }
         private void CreateGrid()
         {
+            DynamicGrid = new Grid();
+            DynamicGrid.Width = 400;
+            DynamicGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            DynamicGrid.VerticalAlignment = VerticalAlignment.Top;
+            DynamicGrid.ShowGridLines = true;
+            DynamicGrid.Background = new SolidColorBrush(Colors.LightSeaGreen);
+            
+            CreateColumns();
+        }
+        private void CreateColumns()
+        {
+            ColumnDefinition gridCol1 = new ColumnDefinition();
+            ColumnDefinition gridCol2 = new ColumnDefinition();
+            ColumnDefinition gridCol3 = new ColumnDefinition();
 
+            DynamicGrid.ColumnDefinitions.Add(gridCol1);
+            DynamicGrid.ColumnDefinitions.Add(gridCol2);
+            DynamicGrid.ColumnDefinitions.Add(gridCol3);
+
+            CreateRows();
+        }
+        private void CreateRows()
+        {
+            RowDefinition gridRow1 = new RowDefinition();
+            gridRow1.Height = new GridLength(45);
+
+            RowDefinition gridRow2 = new RowDefinition();
+            gridRow2.Height = new GridLength(45);
+
+            RowDefinition gridRow3 = new RowDefinition();
+            gridRow3.Height = new GridLength(45);
+
+            DynamicGrid.RowDefinitions.Add(gridRow1);
+            DynamicGrid.RowDefinitions.Add(gridRow2);
+            DynamicGrid.RowDefinitions.Add(gridRow3);
         }
     }
 }
